c632a1c6da18d25355e025da916ad223de391cb9
rbygrave <=>
2012-09-14 21:23:43 +1200

    Change to LICENSE.TXT

--- src/main/java/com/avaje/ebean/enhance/ant/TransformationListener.java
+++ src/main/java/com/avaje/ebean/enhance/ant/TransformationListener.java
@@ -1,20 +1,22 @@
-/**
- * 
- */
 package com.avaje.ebean.enhance.ant;
 
 /**
  * Mechanism for caller (ANT Task, Maven plugin, Hudson plugin, etc.) to
  * report events back to the user interface.
+ * 
  * @author Paul Mendelson
  *
  */
 public interface TransformationListener {
 
-	/** Report normal processing event */
-	void logEvent(String msg);
+	/** 
+	 * Report normal processing event 
+	 */
+	public void logEvent(String msg);
 
-	/** Report processing "error" */
-	void logError(String msg);
+	/** 
+	 * Report processing "error" 
+	 */
+	public void logError(String msg);
 
 }
