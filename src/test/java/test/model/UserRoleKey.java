97f41c41537cf7a7fd3c46b5dffe2418946dc078
rob <rob@clearfield.com>
2015-05-01 15:33:17 +1200

    Add test for package visibility (not public)

---
+++ src/test/java/test/model/UserRoleKey.java
@@ -0,0 +1,35 @@
+package test.model;
+
+import javax.persistence.Embeddable;
+
+/**
+ * An embedded bean that has package visibility (is not public).
+ */
+@Embeddable
+class UserRoleKey {
+
+    private long userId;
+
+    private int roleId;
+
+    public UserRoleKey(final int roleId, final long userId) {
+        this.roleId = roleId;
+        this.userId = userId;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(final int roleId) {
+        this.roleId = roleId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(final long userId) {
+        this.userId = userId;
+    }
+}
