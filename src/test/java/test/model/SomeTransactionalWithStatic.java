93726052d7ea442a7e1e7b9dd53d1c18303eb5da
Rien <info@nentjes.com>
2018-03-27 00:56:24 +0200

    @Transactional on class levels causes <clinit> to get enhanced (#79)
    * Add class initializer creates transaction test

--- src/test/java/test/model/SomeTransactionalWithStatic.java
+++ src/test/java/test/model/SomeTransactionalWithStatic.java
@@ -3,13 +3,20 @@ package test.model;
 import io.ebean.Ebean;
 import io.ebean.Transaction;
 import io.ebean.annotation.Transactional;
+import io.ebean.annotation.TxType;
 
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
 
+@Transactional(type = TxType.REQUIRED)
 public class SomeTransactionalWithStatic {
 
   static {
     Boolean anything = true;
+
+    Transaction tdTransaction = Ebean.currentTransaction();
+    System.out.println("--- in <clinit> " + tdTransaction);
+    assertNull(tdTransaction);
   }
 
   @Transactional(label = "something")
