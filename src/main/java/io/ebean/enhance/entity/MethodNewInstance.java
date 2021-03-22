acb68835a9b94e82ee1a1e4fdc74b775b0789b39
Robin Bygrave <robin.bygrave@gmail.com>
2021-03-23 11:15:08 +1300

    No functional change - tidy MethodNewInstance with static imports

--- src/main/java/io/ebean/enhance/entity/MethodNewInstance.java
+++ src/main/java/io/ebean/enhance/entity/MethodNewInstance.java
@@ -3,9 +3,9 @@ package io.ebean.enhance.entity;
 import io.ebean.enhance.asm.ClassVisitor;
 import io.ebean.enhance.asm.Label;
 import io.ebean.enhance.asm.MethodVisitor;
-import io.ebean.enhance.asm.Opcodes;
 import io.ebean.enhance.common.ClassMeta;
 
+import static io.ebean.enhance.asm.Opcodes.*;
 import static io.ebean.enhance.common.EnhanceConstants.INIT;
 import static io.ebean.enhance.common.EnhanceConstants.NOARG_VOID;
 
@@ -19,15 +19,15 @@ class MethodNewInstance {
    */
   static void addMethod(ClassVisitor cv, ClassMeta classMeta) {
 
-    MethodVisitor mv = cv.visitMethod(Opcodes.ACC_PUBLIC + Opcodes.ACC_SYNTHETIC, "_ebean_newInstance", "()Ljava/lang/Object;", null, null);
+    MethodVisitor mv = cv.visitMethod(ACC_PUBLIC + ACC_SYNTHETIC, "_ebean_newInstance", "()Ljava/lang/Object;", null, null);
     mv.visitCode();
     Label l0 = new Label();
     mv.visitLabel(l0);
     mv.visitLineNumber(10, l0);
-    mv.visitTypeInsn(Opcodes.NEW, classMeta.getClassName());
-    mv.visitInsn(Opcodes.DUP);
-    mv.visitMethodInsn(Opcodes.INVOKESPECIAL, classMeta.getClassName(), INIT, NOARG_VOID, false);
-    mv.visitInsn(Opcodes.ARETURN);
+    mv.visitTypeInsn(NEW, classMeta.getClassName());
+    mv.visitInsn(DUP);
+    mv.visitMethodInsn(INVOKESPECIAL, classMeta.getClassName(), INIT, NOARG_VOID, false);
+    mv.visitInsn(ARETURN);
 
     Label l1 = new Label();
     mv.visitLabel(l1);
