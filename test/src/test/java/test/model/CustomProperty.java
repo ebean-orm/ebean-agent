5b23817f23dc9a7bbbe0713739aeff689459da5a
Rob Bygrave <robin.bygrave@gmail.com>
2022-04-28 09:21:13 +1200

    #180 - Get test to fail by bumping  ebean-version.mf ebean-version to 133

--- test/src/test/java/test/model/CustomProperty.java
+++ test/src/test/java/test/model/CustomProperty.java
@@ -2,18 +2,13 @@ package test.model;
 
 import io.ebean.annotation.DocStore;
 
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.Inheritance;
-
 @DocStore
-@Inheritance
-@DiscriminatorColumn(name = "kind")
 public abstract class CustomProperty<T> {
 
   public abstract Object getValue();
 
   @Override
-  public final String toString() {
+  public String toString() {
     return String.valueOf(getValue());
   }
 
